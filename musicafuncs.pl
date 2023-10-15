10b0216245d30f56e961b523b07bcd7fa516f80b
DovCaio <onlydeadnoalive@gmail.com>
2023-10-14 23:13:18 -0300

    Implementado as funcionalidade de percistencia de musicas.

--- musicafuncs.pl
+++ musicafuncs.pl
@@ -1,13 +1,43 @@
+:- use_module(library(csv)).
 :-dynamic(musica/8).
+:-initialization(carregaMusicas).
+
+carregaMusicas :- 
+    consult('musicas.pl').
 
 
+salvarMusicas :-
+    open('musicas.pl', write, Stream),
+    forall(musica(Nome, Instrumentos, Participantes, Ritmo, DataLancamento, Letra, NomeBanda, Avaliacao),
+           format(Stream, 'musica(~q, ~q, ~q, ~q, ~q, ~q, ~q, ~q).~n', 
+                  [Nome, Instrumentos, Participantes, Ritmo, DataLancamento, Letra, NomeBanda, Avaliacao])),
+    close(Stream).
 
 adicionaMusica(Nome, Instrumentos, Participantes, Ritmo, DataLancamento, Letra, NomeBanda, Avaliacao) :-
-    asserta(musica(Nome, Instrumentos, Participantes, Ritmo, DataLancamento, Letra, NomeBanda, Avaliacao)).
+    asserta(musica(Nome, Instrumentos, Participantes, Ritmo, DataLancamento, Letra, NomeBanda, Avaliacao)),
+    salvarMusicas.
 
 selecionaMusica(Id, R) :-
- musica(Nome, Instrumentos, Participantes, Ritmo, DataLancamento, Letra, NomeBanda, Avaliacao),
- R = musica(Nome, Instrumentos, Participantes, Ritmo, DataLancamento, Letra, NomeBanda, Avaliacao) .
+ musica(Id, Instrumentos, Participantes, Ritmo, DataLancamento, Letra, NomeBanda, Avaliacao),R = musica(Id, Instrumentos, Participantes, Ritmo, DataLancamento, Letra, NomeBanda, Avaliacao).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+printarMusicas :-
+    findall((Nome, Instrumentos, Participantes, Ritmo, DataLancamento, Letra, NomeBanda, Avaliacao),
+            musica(Nome, Instrumentos, Participantes, Ritmo, DataLancamento, Letra, NomeBanda, Avaliacao),
+            Musicas),
+    printaElas(Musicas).
 
 
 
