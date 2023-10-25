b750d74758694cc5b56a1ed576e359e473454893
Costa Joseff <rielisson.joseff.costa@ccc.ufcg.edu.br>
2023-10-24 23:14:17 -0300

    Sorts bem definitos e tentativa de exibição do dash ainda com bugs

--- util.pl
+++ util.pl
@@ -16,3 +16,47 @@ splitVS(String, Retorno) :-
 limitarCasasDecimais(Numero, NumeroFormatado) :-
   atomic_list_concat(['~2f', Numero], '', Formato),
   format(atom(NumeroFormatado), Formato, [Numero]).
+
+% Ordena a lista de artistas de forma decrescente
+ordenarArtistas([], Temp, Temp).
+ordenarArtistas([H|T], Temp, Sorted) :-
+  inserirArtistaOrdenado(H, Temp, [], InseridoH),
+  ordenarArtistas(T, InseridoH, Sorted).
+
+inserirArtistaOrdenado(Alvo, [], [], [Alvo]).
+inserirArtistaOrdenado(Alvo, [H|T], Temp, Resultado):-
+  nth0(5, Alvo, AvaliacaoAlvo),
+  nth0(5, H, AvaliacaoH),
+  AvaliacaoAlvo < AvaliacaoH,
+  append(Temp, [H], Esquerdo),
+  inserirArtistaOrdenado(Alvo, T, Esquerdo, Resultado).
+inserirArtistaOrdenado(Alvo, [H|T], Temp, Resultado):-
+  nth0(5, Alvo, AvaliacaoAlvo),
+  nth0(5, H, AvaliacaoH),
+  AvaliacaoAlvo >= AvaliacaoH,
+  append(Temp, [Alvo], X),
+  append(X, [H|T], Resultado).
+inserirArtistaOrdenado(Alvo, [], Temp, Resultado):-
+  append(Temp, [Alvo], Resultado).
+
+% Ordena a lista de musicas de forma decrescente
+ordenarMusicas([], Temp, Temp).
+ordenarMusicas([H|T], Temp, Sorted):-
+  inserirMusicaOrdenado(H, Temp, [], InseridoH),
+  ordenarMusicas(T, InseridoH, Sorted).
+
+inserirMusicaOrdenado(Alvo, [], [] ,[Alvo]).
+inserirMusicaOrdenado(Alvo, [H|T], Temp, Resultado):-
+  nth0(8, Alvo, AvaliacaoAlvo),
+  nth0(8, H, AvaliacaoH),
+  AvaliacaoAlvo < AvaliacaoH,
+  append(Temp, [H], Esquerdo),
+  inserirMusicaOrdenado(Alvo, T, Esquerdo, Resultado).
+inserirMusicaOrdenado(Alvo, [H|T], Temp, Resultado):-
+  nth0(8, Alvo, AvaliacaoAlvo),
+  nth0(8, H, AvaliacaoH),
+  AvaliacaoAlvo >= AvaliacaoH,
+  append(Temp, [Alvo], X),
+  append(X, [H|T], Resultado).
+inserirMusicaOrdenado(Alvo, [], Temp, Resultado):-
+  append(Temp, [Alvo], Resultado).
