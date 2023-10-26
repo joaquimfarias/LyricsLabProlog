745d2d1236b4143fd8de53c5d1875cada76e9cf8
joaquimfarias <joaquim.souza@ccc.ufcg.edu.br>
2023-10-25 21:30:12 -0300

    adição funçoes banda

---
+++ bandas.pl
@@ -0,0 +1,46 @@
+:- dynamic banda/7.
+banda('Nome da Banda', ['Membro1', 'Membro2', 'Membro3'], ['AntigoMembro1'], ['Música1', 'Música2'], ['Instrumento1', 'Instrumento2'], 'Data', 'Gênero').
+banda('The Beatles', ['John Lennon', 'Paul McCartney', 'George Harrison', 'Ringo Starr'], [], ['Hey Jude', 'Let It Be', 'Yesterday'], ['Guitar', 'Bass', 'Guitar', 'Drums'], '1960', 'Rock').
+banda('Pink Floyd', ['Roger Waters', 'David Gilmour', 'Richard Wright', 'Nick Mason'], [], ['Comfortably Numb', 'Wish You Were Here', 'Money'], ['Guitar', 'Guitar', 'Keyboard', 'Drums'], '1965', 'Rock').
+banda('Queen', ['Freddie Mercury', 'Brian May', 'John Deacon', 'Roger Taylor'], [], ['Bohemian Rhapsody', 'We Will Rock You', 'Radio Ga Ga'], ['Vocals', 'Guitar', 'Bass', 'Drums'], '1970', 'Rock').
+banda('Slipknot', ['Corey Taylor', 'Jim Root', 'Shawn Crahan', 'Jay Weinberg', 'Alessandro Venturella', 'Sid Wilson', 'Craig Jones', 'Tortilla Man'], ['Joey Jordison', 'Paul Gray'], ['Psychosocial', 'Duality', 'Before I Forget'], ['Vocals', 'Guitar', 'Percussion', 'Drums', 'Bass', 'Turntables', 'Samples', 'Keyboards'], '1995', 'Metal').
+
+% Inicializa uma lista vazia para armazenar as bandas.
+:- dynamic bandas/1.
+assertz(bandas([])).
+
+% Predicado para adicionar uma banda
+adicionar_banda(Nome, Membros, ArtistasAnteriores, Musicas, Instrumentos, DataFundacao, Genero) :-
+    retract(bandas(Lista)),
+    assertz(bandas([[Nome, Membros, ArtistasAnteriores, Musicas, Instrumentos, DataFundacao, Genero] | Lista])).
+
+% Predicado para listar todas as bandas
+listar_banda :-
+    banda(Nome, Membros, ArtistasAnteriores, Musicas, Instrumentos, DataFundacao, Genero),
+    format("Nome: ~w~nComposição Atual: ~w~nArtistas Anteriores: ~w~nMúsicas: ~w~nInstrumentos: ~w~nData de Fundação: ~w~nGênero: ~w~n~n",
+        [Nome, Membros, ArtistasAnteriores, Musicas, Instrumentos, DataFundacao, Genero]).
+
+% Predicado para listar todas as bandas
+listar_bandas :-
+    bandas(Lista),
+    write("Lista de bandas:\n"),
+    listar_bandas_aux(Lista).
+
+listar_bandas_aux([]).
+listar_bandas_aux([Banda | Resto]) :-
+    write(Banda), nl,
+    listar_bandas_aux(Resto).
+
+% Predicado para obter uma banda da lista
+get_banda(NomeBanda, Banda) :-
+    bandas(Lista),
+    member([NomeBanda, Membros, ArtistasAnteriores, Musicas, Instrumentos, DataFundacao, Genero], Lista),
+    Banda = [NomeBanda, Membros, ArtistasAnteriores, Musicas, Instrumentos, DataFundacao, Genero].
+
+% Predicado para definir uma banda na lista
+set_banda(NomeBanda, NovaBanda) :-
+    bandas(Lista),
+    subtract(Lista, [[NomeBanda, _, _, _, _, _, _]], ListaAtual), % Remove a banda existente (se houver)
+    append(ListaAtual, [NovaBanda], NovaLista), % Adiciona a nova banda
+    retract(bandas(_)), % Remove a lista de bandas atual
+    assertz(bandas(NovaLista)). % Define a nova lista de bandas
---
+++ bandasfuncs.pl
@@ -0,0 +1,95 @@
+:- consult('bandas.pl')
+
+% Predicado para salvar uma banda
+salva_banda(Banda) :-
+    assertz(banda(Banda)), % Adicionar a banda à base de conhecimento.
+    writeln('Banda salva com sucesso.').
+    
+% Predicado para listar todas as bandas
+listar_bandas :-
+    findall(B, banda(B), Bandas), % Coletar todas as bandas da base de conhecimento.
+    listar_bandas_aux(Bandas).
+
+listar_bandas_aux([]).
+listar_bandas_aux([Banda | Resto]) :-
+    writeln(Banda), % Exibir a banda.
+    listar_bandas_aux(Resto).
+
+% Predicado para recuperar uma banda por nome
+recuperar_por_nome(NomeBanda, Banda) :-
+    banda(Banda), % Verificar se a banda existe na base de conhecimento.
+    nome(Banda, NomeBanda), % Verificar se o nome da banda corresponde ao fornecido.
+    writeln('Banda encontrada.').
+recuperar_por_nome(_, Banda) :-
+    writeln('Banda não encontrada.'),
+    Banda = banda('null', ['null', 'null'], ['null'], ['null'], ['null'], 'null', 'null').
+
+% Predicado para verificar se uma banda existe
+existe_banda(NomeBanda) :-
+    banda(Banda), % Verificar se a banda existe na base de conhecimento.
+    nome(Banda, NomeBanda), % Verificar se o nome da banda corresponde ao fornecido.
+    writeln('A banda existe.').
+existe_banda(_) :-
+    writeln('A banda não existe.').
+
+% Predicado para adicionar um instrumento a uma banda
+adicionar_instrumento(Instrumento, NomeBanda) :-
+    banda(Banda), % Verificar se a banda existe na base de conhecimento.
+    nome(Banda, NomeBanda), % Verificar se o nome da banda corresponde ao fornecido.
+    instrumentos(Banda, InstrumentosAtuais),
+    append(InstrumentosAtuais, [Instrumento], NovosInstrumentos),
+    retract(banda(Banda)), % Remover a versão anterior da banda da base de conhecimento.
+    assertz(banda(banda(NomeBanda, Compositores, Musicos, Musicas, NovosInstrumentos, DataFundacao, Genero))),
+    writeln('Instrumento adicionado com sucesso à banda.').
+adicionar_instrumento(_, _) :-
+    writeln('Banda não encontrada ou instrumento não adicionado.').
+
+% Predicado para adicionar um novo integrante a uma banda
+adicionar_integrante(Integrante, NomeBanda) :-
+    banda(Banda), % Verificar se a banda existe na base de conhecimento.
+    nome(Banda, NomeBanda), % Verificar se o nome da banda corresponde ao fornecido.
+    composicao_atual(Banda, IntegrantesAtuais),
+    append(IntegrantesAtuais, [Integrante], NovosIntegrantes),
+    retract(banda(Banda)), % Remover a versão anterior da banda da base de conhecimento.
+    assertz(banda(banda(NomeBanda, Compositores, NovosIntegrantes, Musicas, Instrumentos, DataFundacao, Genero))),
+    writeln('Integrante adicionado com sucesso à banda.').
+adicionar_integrante(_, _) :-
+    writeln('Banda não encontrada ou integrante não adicionado.').
+
+% Predicado para remover um integrante de uma banda
+remover_integrante(Integrante, NomeBanda) :-
+    banda(Banda), % Verificar se a banda existe na base de conhecimento.
+    nome(Banda, NomeBanda), % Verificar se o nome da banda corresponde ao fornecido.
+    composicao_atual(Banda, IntegrantesAtuais),
+    delete(IntegrantesAtuais, Integrante, NovosIntegrantes),
+    retract(banda(Banda)), % Remover a versão anterior da banda da base de conhecimento.
+    assertz(banda(banda(NomeBanda, Compositores, NovosIntegrantes, Musicas, Instrumentos, DataFundacao, Genero))),
+    writeln('Integrante removido com sucesso da banda.').
+remover_integrante(_, _) :-
+    writeln('Banda não encontrada ou integrante não removido.').
+
+% Predicado para remover uma banda
+remover_banda(NomeBanda) :-
+    retract(banda(banda(NomeBanda, _, _, _, _, _, _))), % Remover a banda da base de conhecimento.
+    writeln('Banda removida com sucesso.').
+remover_banda(_) :-
+    writeln('Banda não encontrada ou não removida.').
+
+% Predicado para atualizar uma banda
+atualizar_banda(NomeBanda, BandaNova) :-
+    retract(banda(banda(NomeBanda, _, _, _, _, _, _))), % Remover a versão anterior da banda da base de conhecimento.
+    assertz(BandaNova), % Adicionar a nova versão da banda à base de conhecimento.
+    writeln('Banda atualizada com sucesso.').
+
+% Predicado para filtrar bandas por instrumento
+filtrar_bandas_por_instrumento(Instrumento, BandasFiltradas) :-
+    findall(B, (banda(B), instrumentos(B, Instrumentos), member(Instrumento, Instrumentos)), BandasFiltradas).
+
+% Predicado para filtrar bandas por gênero
+filtrar_bandas_por_genero(Genero, BandasFiltradas) :-
+    findall(B, (banda(B), genero(B, Genero)), BandasFiltradas).
+
+% Predicado para filtrar bandas por artista
+filtrar_bandas_por_artista(Artista, BandasFiltradas) :-
+    findall(B, (banda(B), (composicao_atual(B, IntegrantesAtual); artistas_anteriores(B, IntegrantesAnteriores)),
+        member(Artista, IntegrantesAtual); member(Artista, IntegrantesAnteriores)), BandasFiltradas).
